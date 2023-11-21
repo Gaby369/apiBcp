8b34f2bed1d50ed3df7cd8d55d2dd58909763f6f
Gabi369 <limachig2@gmail.com>
2023-11-21 12:20:58 -0400

    Agregar archivos de proyecto.

---
+++ BCP/Models/Tables/Cuentum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BCP.Models.Tables
+{
+    public partial class Cuentum
+    {
+        public int IdIn { get; set; }
+        public int? FkClienteIdIn { get; set; }
+        public string? NombreCuentaVc { get; set; }
+        public string? TipoCuentaVc { get; set; }
+        public string? TipoMonedaVc { get; set; }
+        public string? SucursalVc { get; set; }
+        public DateTime? FechaRegistroDt { get; set; }
+        public string? UsaurioRegistroVc { get; set; }
+    }
+}
